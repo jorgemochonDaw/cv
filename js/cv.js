fab8c4a908c1ed258260256e16e1dbe17d9f5def
jorgemochonDaw <bernalmochonjorge@gmail.com>
2021-11-01 19:37:02 +0100

    d

--- js/cv.js
+++ js/cv.js
@@ -4,6 +4,7 @@ const btnFormulario3 = document.querySelector('#formulario3');
 const btnFormulario4 = document.querySelector('#formulario4');
 const btnFormulario5 = document.querySelector('#formulario5');
 const btnCrear = document.querySelector('#btn-crear');
+const show = document.querySelector('#show');
 
 eventos();
 
@@ -17,10 +18,12 @@ function eventos() {
 
 function formulario1(e) {
     e.preventDefault();
-    let section1 = e.target.parentElement.parentElement.parentElement;
+    let section1 = e.target.parentElement.parentElement;
+    console.log(e.target.parentElement.parentElement);
     if (section1.classList.contains('mostrar')) {
         section1.remove('mostrar');
-        let section2 = btnFormulario2.parentElement.parentElement.parentElement;
+        console.log(btnFormulario2.parentElement);
+        let section2 = btnFormulario2.parentElement.parentElement;
         section2.classList.remove('ocultar');
         section2.classList.add('mostrar');
     }
@@ -28,10 +31,10 @@ function formulario1(e) {
 
 function formulario2(e) {
     e.preventDefault();
-    let section2 = e.target.parentElement.parentElement.parentElement;
+    let section2 = e.target.parentElement.parentElement;
     if (section2.classList.contains('mostrar')) {
         section2.remove('ocultar');
-        let section3 = btnFormulario3.parentElement.parentElement.parentElement;
+        let section3 = btnFormulario3.parentElement.parentElement;
         section3.classList.remove('ocultar');
         section3.classList.add('mostrar');
     }
@@ -39,10 +42,10 @@ function formulario2(e) {
 
 function formulario3(e) {
     e.preventDefault();
-    let section3 = e.target.parentElement.parentElement.parentElement;
+    let section3 = e.target.parentElement.parentElement;
     if (section3.classList.contains('mostrar')) {
         section3.remove('ocultar');
-        let section4 = btnFormulario4.parentElement.parentElement.parentElement;
+        let section4 = btnFormulario4.parentElement.parentElement;
         section4.classList.remove('ocultar');
         section4.classList.add('mostrar');
     }
@@ -50,10 +53,10 @@ function formulario3(e) {
 
 function formulario4(e) {
     e.preventDefault();
-    let section4 = e.target.parentElement.parentElement.parentElement;
+    let section4 = e.target.parentElement.parentElement;
     if (section4.classList.contains('mostrar')) {
         section4.remove('ocultar');
-        let section5 = btnFormulario5.parentElement.parentElement.parentElement;
+        let section5 = btnFormulario5.parentElement.parentElement;
         section5.classList.remove('ocultar');
         section5.classList.add('mostrar');
     }
@@ -61,11 +64,20 @@ function formulario4(e) {
 
 function formulario5(e) {
     e.preventDefault();
-    let section5 = e.target.parentElement.parentElement.parentElement;
+    let section5 = e.target.parentElement.parentElement;
     if (section5.classList.contains('mostrar')) {
         section5.remove('ocultar');
         let section6 = btnCrear.parentElement;
         section6.classList.remove('ocultar');
         section6.classList.add('mostrar');
+        setTimeout(() => {
+            let input = document.createElement('input');
+            input.type = 'submit';
+            input.name = 'crearCv';
+            input.value = 'Generar Curriculum';
+            input.classList.add('crearCv');
+            btnCrear.remove();
+            show.append(input);
+        }, 3000);
     }
 }
